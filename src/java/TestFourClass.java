9a11fa88d697972ed57203af1b72ad38bd9e2f90
Vitaliy-Katilov <vitaliy.ka-75@yandex.ru>
2023-04-04 20:30:53 +0300

    commit2 Test4 Lesson1

--- src/java/TestFourClass.java
+++ src/java/TestFourClass.java
@@ -1,6 +1,6 @@
 public class TestFourClass {
     public static void main(String[] args) {
-        String str = "Test four lesson one";
+        String str = "Test four lesson one changed";
         System.out.println(str);
     }
 }
