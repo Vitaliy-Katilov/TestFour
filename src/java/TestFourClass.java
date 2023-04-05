5dddabb5a5db3e3323c3714d99f82f95dee8fdd2
Vitaliy-Katilov <vitaliy.ka-75@yandex.ru>
2023-04-05 13:04:46 +0300

    commit twoMessage

--- src/java/TestFourClass.java
+++ src/java/TestFourClass.java
@@ -2,5 +2,7 @@ public class TestFourClass {
     public static void main(String[] args) {
         String str = "Test four lesson one changed";
         System.out.println(str);
+        String str2 = "Test five lesson one changed";
+        System.out.println(str2);
     }
 }
